98830c59900213639c6456abf4d9bbe2c1d63eca
Marcos Renann <marcos.renann.br@gmail.com>
2021-04-29 19:29:37 -0300

    HomeFragment Configs

--- app/src/main/java/com/mrenann/notes/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/mrenann/notes/view/fragment/HomeFragment.kt
@@ -1,15 +1,27 @@
 package com.mrenann.notes.view.fragment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.mrenann.notes.R
+import com.mrenann.notes.adapter.NotesAdapter
+import com.mrenann.notes.database.NotesDatabase
 import com.mrenann.notes.databinding.FragmentHomeBinding
+import com.mrenann.notes.entities.Notes
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
     private lateinit var binding: FragmentHomeBinding
+    var arrNotes = ArrayList<Notes>()
+
+    private val notesAdapter: NotesAdapter by lazy { NotesAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +34,52 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.setHasFixedSize(true)
+
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+
+        launch {
+            context?.let {
+                var notes = NotesDatabase.getDatabase(it).noteDao().getAllNotes()
+                notesAdapter.setData(notes)
+                arrNotes = notes as ArrayList<Notes>
+                binding.recyclerView.adapter = notesAdapter
+            }
+        }
+
+        binding.fabBtnCreateNote.setOnClickListener {
+            replaceFragment(CreateNoteFragment(),false)
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val tempArr = ArrayList<Notes>()
+
+                for (arr in arrNotes){
+                    if (arr.title!!.toLowerCase(Locale.getDefault()).contains(newText.toString())){
+                        tempArr.add(arr)
+                    }
+                }
+
+                notesAdapter.setData(tempArr)
+                notesAdapter.notifyDataSetChanged()
+                return true
+            }
+        })
     }
+
+    private fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = activity?.supportFragmentManager?.beginTransaction()
+
+        if (istransition){
+            fragmentTransition?.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition?.replace(R.id.frame_layout,fragment)?.addToBackStack(fragment.javaClass.simpleName)?.commit()
+    }
+
 }
