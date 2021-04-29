30ee85ab9aa68a24f5c73e6b5e60047bae00a1ae
Marcos Renann <marcos.renann.br@gmail.com>
2021-04-29 16:56:04 -0300

    Create and Config Home Fragment

---
+++ app/src/main/java/com/mrenann/notes/view/fragment/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.mrenann.notes.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mrenann.notes.R
+import com.mrenann.notes.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
